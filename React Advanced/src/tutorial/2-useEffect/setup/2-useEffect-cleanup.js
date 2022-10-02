8a545cb47ece62a5de01389b577c338fc85ef608
Harsh Bhadu <102225113+harshhh11@users.noreply.github.com>
2022-10-02 16:22:12 +0530

    Update 2-useEffect-cleanup.js

--- React Advanced/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ React Advanced/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -5,16 +5,20 @@ import React, { useState, useEffect } from 'react';
 
 const UseEffectCleanup = () => {
   const [size, setSize] = useState(window.innerWidth)
+  
   const updateSize = () => {
     setSize(window.innerWidth);
   };
+  
+  //we can return a function in useEffect known as cleanup
+  //through this we can clean any event listeners if we want to avoid memory leaks  
   useEffect(() => {
     console.log("useEffect")
     window.addEventListener('resize', updateSize)
-    // return () => {
-    //   console.log('Cleanup')
-    //   window.removeEventListener('resize', updateSize)
-    // }
+    return () => {
+      console.log('Cleanup')
+      window.removeEventListener('resize', updateSize)
+    }
   }, [])
   console.log('render')
   return (
