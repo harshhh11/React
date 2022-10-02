9f2df3f6c73220aec9fd1a800952728ae4b4732b
Harsh Bhadu <102225113+cookieee1@users.noreply.github.com>
2022-10-02 11:37:47 +0530

    Update index.js

--- React Basics/src/index.js
+++ React Basics/src/index.js
@@ -9,10 +9,12 @@ import Book from './Book';
 
 function BookList() {
   return (
-    <section className="booklist">{books.map((book) => {
+    <section className="booklist">
+      {books.map((book) => {
       // return <Book key={book.id} book={book}></Book>;
       return <Book key={book.id} {...book}></Book>;
-    })}</section>
+      })}
+    </section>
   );
 }
 
